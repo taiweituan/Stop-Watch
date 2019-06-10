c08ed65f5ab74a8c631f4d72f7df54729302395b
Taiwei Tuan <taiweituan@gmail.com>
2019-06-10 10:30:40 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state= {
+      timer: 0
+    };
+    this.interval = () => {};
+    this.handleStart = this.handleStart.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleStart(){
+    console.log('start');
+    this.interval = setInterval(()=> {
+      let currentTimer = this.state.timer;
+      this.setState({
+        timer: currentTimer+1
+      });
+    }, 1000);
+  }
+  handleStop(){
+    console.log('stop');
+    clearInterval(this.interval);
+  }
+
+  handleReset() {
+    console.log('reset');
+    this.setState({
+      timer: 0
+    });
+  }
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stop Watch</h1>
+      <h2>{this.state.timer}</h2>
+      <button onClick={
+        this.handleStart
+      }>Start</button>
+      <button onClick={this.handleStop}>Stop</button>
+      <button onClick={this.handleReset}>Reset</button>
     </div>
   );
 }
+}
 
 export default App;
+
